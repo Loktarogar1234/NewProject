541f14900df99de5f5eb4672516ab9e1929cdacd
Evgeny <evmixa@gmail.com>
2024-04-20 12:42:52 +0300

    threadses

---
+++ src/Exeptions/throwses.java
@@ -0,0 +1,20 @@
+package Exeptions;
+
+import java.io.IOException;
+
+public class throwses {
+    public static void main(String[] args) {
+
+        //private static void funcs();
+        {
+        }
+    }
+/*
+    private static void funcs() {
+        //throw new Exception("My Exeption");
+
+        Exeption e = new Exception();
+        */
+
+    }
+
---
+++ src/SomeThreads.java
@@ -0,0 +1,61 @@
+ class threadses {
+
+     public void howToRunThreads() {
+         ThreadClass threadClass = new ThreadClass("First");
+         threadClass.start(); //method ThreadClass.run()
+
+         Thread thread = new Thread(new RunnableClass("Second"));
+         Thread thread2 = new Thread(new RunnableClass("Third"));
+         Thread thread3 = new Thread(new RunnableClass("Fourth"));
+         thread.start(); //method RunnableClass.run()
+         thread2.start(); //method RunnableClass.run()
+         thread3.start(); //method RunnableClass.run()
+     }
+
+     public class RunnableClass implements Runnable {
+
+         private String localName;
+
+         public RunnableClass() {
+         }
+
+         public RunnableClass(String localName) {
+             this.localName = localName;
+         }
+
+         @Override
+         public void run() {
+             System.out.println("run() " + localName + " running");
+         }
+
+         public String getLocalName() {return localName;}
+         public void setLocalName(String localName) {this.localName = localName;}
+     }
+
+     public class ThreadClass extends Thread {
+
+         public ThreadClass() {
+         }
+
+         public ThreadClass(String name) {
+             super(name);
+         }
+
+         public ThreadClass(Runnable target) {
+             super(target);
+             System.out.println(target + " will running");
+         }
+
+         @Override
+         public void run() {
+             System.out.println("ThreadClass run() method " + "Thread name is: " + this.getName());
+         }
+     }
+     /*/Выведение
+     ThreadClass run() method Thread name is: First
+     run() Third running
+     run() Fourth running
+     run() Second running
+     */
+
+}
