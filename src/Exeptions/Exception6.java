231d1d9303960a3feb3b453b333927c5901ab3c7
Loktarogar1234 <145042096+Loktarogar1234@users.noreply.github.com>
2024-04-21 14:48:56 +0300

    правки

--- src/Exeptions/Exception6.java
+++ src/Exeptions/Exception6.java
@@ -1,11 +1,8 @@
 package Exeptions;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
 
-public class Exeption6 {
+public class Exception6 {
 
     public static void main(String[] args) {
 
--- src/throwses.java
+++ src/throwses.java
@@ -1,5 +1,3 @@
-package Exeptions;
-
 import java.io.IOException;
 
 public class throwses {
